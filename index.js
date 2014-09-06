4d3be2c92e4a51fda09104012b00d4d262b4271c
Eric Lathrop <eric@ericlathrop.com>
2014-09-05 22:52:34 -0400

    Better error reporting.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 "use strict";
 
+var http = require("http");
 var https = require("https");
 var url = require("url");
 var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+var path = require("path");
+var exec = Promise.promisify(require("child_process").exec);
 
 function httpsGet(options) {
 	return new Promise(function(resolve, reject) {
 		https.get(options, function(res) {
+			if (res.statusCode >= 400) {
+				promisifyReadableStream(res).then(function(data) {
+					reject(res.statusCode + " " + http.STATUS_CODES[res.statusCode] + "\n" + data);
+				}, function(err) {
+					reject(err);
+				});
+				return;
+			}
 			resolve(res);
 		}).on("error", function(err) {
 			reject(err);
@@ -29,7 +41,8 @@ function promisifyReadableStream(stream) {
 function get(path) {
 	var options = url.parse("https://api.github.com" + path);
 	options.headers = {
-		"User-Agent": "github-backup"
+		"User-Agent": "github-backup",
+		"Accept": "application/vnd.github.v3+json"
 	};
 
 	return httpsGet(options).then(promisifyReadableStream).then(JSON.parse);
@@ -48,11 +61,44 @@ function getOrgRepos(org) {
 }
 
 function backupRepo(repo) {
-	var re = new RegExp("https://github\.com/([^/]+)/([^/]+)\.git");
-	var matches = repo.clone_url.match(re);
+	var url = repo.clone_url;
+	var re = new RegExp("https://github\.com/([^/]+)/([^/]+)");
+	var matches = url.match(re);
 	var user = matches[1];
 	var repoName = matches[2];
-	console.log(repo.clone_url, user, repoName);
+	var repoPath = path.join(user, repoName);
+	// console.log(url, user, repoName);
+
+	return fs.statAsync(user).error(function() {
+		return fs.mkdirAsync(user);
+	}).then(function() {
+		return fs.statAsync(repoPath);
+	}).then(function() {
+		console.log("updating", url);
+		return exec("git remote update", { cwd: repoPath });
+	}, function(err) {
+		console.log("cloning", url);
+		return exec("git clone --mirror " + url + " " + repoPath);
+	});
 }
 
-getPublicUserRepos("ericlathrop").each(backupRepo);
+getPublicUserRepos("ericlathrop").then(function(repos) {
+	var promise;
+	for (var i = 0; i < repos.length; i++) {
+		var repo = repos[i];
+		if (promise) {
+			promise = promise.then(function(stdout) {
+				console.log("finished");
+				console.log(stdout);
+				return backupRepo(repo);
+			}, function(err) {
+				console.log("backupRepo err", err);
+			});
+		} else {
+			promise = backupRepo(repo);
+		}
+	}
+	return promise;
+}, function(err) {
+	console.error("Unhandled error:", err);
+}).done();
