ea08aa596a2f07fcac70156ec914e7ea15cebe55
Eric Lathrop <eric@ericlathrop.com>
2014-09-06 00:47:01 -0400

    Extract github module.

--- bin/github-backup.js
+++ bin/github-backup.js
@@ -9,32 +9,7 @@ var Promise = require("bluebird"); // jshint ignore:line
 var accessToken = process.env.GITHUB_ACCESS_TOKEN;
 
 var promisify = require("../lib/promisify");
-
-var url = require("url");
-function get(path) {
-	var options = url.parse("https://api.github.com" + path);
-	options.headers = {
-		"User-Agent": "github-backup",
-		"Accept": "application/vnd.github.v3+json"
-	};
-	if (accessToken) {
-		options.Authorization = "token " + accessToken;
-	}
-
-	return promisify.httpsGet(options).then(promisify.readableStream).then(JSON.parse);
-}
-
-function getPublicUserRepos(user) {
-	var userRepos = get("/users/" + user + "/repos").all();
-	var orgRepos = get("/users/" + user + "/orgs").map(getOrgRepos).all();
-	return Promise.join(userRepos, orgRepos, function(userRepos, orgRepos) {
-		return userRepos.concat.apply(userRepos, orgRepos);
-	});
-}
-
-function getOrgRepos(org) {
-	return get("/orgs/" + org.login + "/repos");
-}
+var github = require("../lib/github");
 
 var path = require("path");
 var fs = Promise.promisifyAll(require("fs"));
@@ -71,7 +46,7 @@ function backupRepoSerialized(repo, destinationDir, promise) {
 }
 
 function backupPublicUserRepos(username, destinationDir) {
-	getPublicUserRepos(username).then(function(repos) {
+	github.getPublicUserRepos(username).then(function(repos) {
 		var promise;
 		for (var i = 0; i < repos.length; i++) {
 			var repo = repos[i];
